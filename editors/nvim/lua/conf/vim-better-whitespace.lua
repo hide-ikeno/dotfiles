d71c30fc5a98042ab944bccd7eb73f3da06a9e04
Hidekazu Ikeno <hide.ikeno@gmail.com>
2020-09-07 19:45:38 +0900

    nvim: update

--- editors/nvim/lua/conf/vim-better-whitespace.lua
+++ editors/nvim/lua/conf/vim-better-whitespace.lua
@@ -5,7 +5,7 @@ function M.setup()
   vim.g.strip_whitespace_on_save      = 1
   vim.g.show_spaces_that_precede_tabs = 1
   vim.g.better_whitespace_filetypes_blacklist = {
-    'diff', 'gitcommit', 'defx', 'denite', 'qf', 'help', 'markdown', 'which_key'
+    'diff', 'gitcommit', 'defx', 'denite', 'qf', 'help', 'markdown', 'packer', 'which_key'
   }
 
   vim.api.nvim_set_keymap("n", "<Leader>x", "<cmd>StripWhitespace<CR>", {noremap = true, silent = true})
