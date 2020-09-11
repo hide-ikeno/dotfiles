e577865cf4794687ca8f55b2fc62b64aa2526d5c
Hidekazu Ikeno <hide.ikeno@gmail.com>
2020-09-11 11:22:37 +0900

    nvim: update treesitter config

--- editors/nvim/lua/conf/nvim-treesitter.lua
+++ editors/nvim/lua/conf/nvim-treesitter.lua
@@ -2,6 +2,10 @@ local M = {}
 
 function M.config()
   require'nvim-treesitter.configs'.setup{
+    -- Setup
+    -- one of 'all', 'language', or a list of languages
+    ensure_installed = "all",
+    -- [[ Highlight ]] {{{
     highlight = {
       -- temporary disable highlight until
       -- https://github.com/nvim-treesitter/nvim-treesitter/issues/423
@@ -12,16 +16,20 @@ function M.config()
         -- ["foo.bar"] = "Identifier"
       }
     },
+    -- }}}
+    -- [[ Incremental selection ]] {{{
     incremental_selection = {
       enable = true,
       disable = {},
       keymaps = {                       -- mappings for incremental selection (visual mappings)
-        init_selection = 'gnn',         -- maps in normal mode to init the node/scope selection
+        init_selection = "gnn",         -- maps in normal mode to init the node/scope selection
         node_incremental = "grn",       -- increment to the upper named parent
         scope_incremental = "grc",      -- increment to the upper scope (as defined in locals.scm)
         node_decremental = "grm",       -- decrement to the previous node
       }
     },
+    -- }}}
+    -- [[ Refactor ]] {{{
     refactor = {
       highlight_definitions = {
         enable = true
@@ -43,35 +51,43 @@ function M.config()
         }
       }
     },
+    -- }}}
+    -- [[ Text objects ]] {{{
     textobjects = { -- syntax-aware textobjects
-      enable = true,
-      disable = {},
-      keymaps = {
-        -- ["iL"] = { -- you can define your own textobjects directly here
-        --   python = "(function_definition) @function",
-        --   cpp = "(function_definition) @function",
-        --   c = "(function_definition) @function",
-        --   java = "(method_declaration) @function"
-        -- },
-        -- or you use the queries from supported languages with textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["aC"] = "@class.outer",
-        ["iC"] = "@class.inner",
-        ["ac"] = "@conditional.outer",
-        ["ic"] = "@conditional.inner",
-        ["ae"] = "@block.outer",
-        ["ie"] = "@block.inner",
-        ["al"] = "@loop.outer",
-        ["il"] = "@loop.inner",
-        ["is"] = "@statement.inner",
-        ["as"] = "@statement.outer",
-        ["ad"] = "@comment.outer",
-        ["am"] = "@call.outer",
-        ["im"] = "@call.inner"
+      select = {
+        enable = true,
+        disable = {},
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["aC"] = "@class.outer",
+          ["iC"] = "@class.inner",
+          ["ac"] = "@conditional.outer",
+          ["ic"] = "@conditional.inner",
+          ["ae"] = "@block.outer",
+          ["ie"] = "@block.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+          ["is"] = "@statement.inner",
+          ["as"] = "@statement.outer",
+          ["am"] = "@call.outer",
+          ["im"] = "@call.inner",
+          ["ad"] = "@comment.outer",
+          ["id"] = "@comment.inner",
+          -- Or you can define your own textobjects like this
+          -- [[
+          ["iF"] = {
+            python = "(function_definition) @function",
+            cpp = "(function_definition) @function",
+            c = "(function_definition) @function",
+            java = "(method_declaration) @function",
+          },
+          -- ]]
+        }
       }
     },
-    ensure_installed = 'all' -- one of 'all', 'language', or a list of languages
+    -- }}}
   }
 end
 
