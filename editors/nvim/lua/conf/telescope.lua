27d9d4d18df67373d26963796a5e76d9e74f1640
Hidekazu Ikeno <hide.ikeno@gmail.com>
2020-10-06 14:41:33 +0900

    nvim: update telescope config

--- editors/nvim/lua/conf/telescope.lua
+++ editors/nvim/lua/conf/telescope.lua
@@ -1,12 +1,44 @@
 local M = {}
 
-function M.setup()
-  -- variables
+function M.find_files()
+  -- command for find file
+  local ignore_globs = {
+    ".git", ".ropeproject", "__pycache__", ".venv", "venv",
+    "node_modules", "images", "*.min.*", "img", "fonts"
+  }
+  local cmd = nil;
+  if vim.fn.executable("fd") then
+    cmd = {"fd", ".", "--hidden", "--type", "f"}
+    for _, x in ipairs(ignore_globs) do
+      table.insert(cmd, "--exclude")
+      table.insert(cmd, x)
+    end
+  elseif vim.fn.executable("rg") then
+    cmd = {"rg", "--follow", "--hidden", "--files"}
+    for _, x in ipairs(ignore_globs) do
+      table.insert(cmd, "--glob=!" .. x)
+    end
+  elseif vim.fn.executable("ag") then
+    cmd = {"ag", "-U", "--hidden", "--follow"}
+    for _, x in ipairs(ignore_globs) do
+      table.insert(cmd, "--exclude=" .. x)
+    end
+    for _, x in ipairs({"--nocolor", "--nogroup", "-g", ""}) do
+      table.insert(cmd, x)
+    end
+  end
 
+  require'telescope.builtin'.find_files{
+    find_command = cmd
+  }
+end
+
+function M.setup()
   -- key mappings
   local opts = {noremap = true, silent = true}
   vim.api.nvim_set_keymap("n", "<Space>b", "<cmd>lua require'telescope.builtin'.buffers{ shorten_path = true }<CR>", opts)
-  vim.api.nvim_set_keymap("n", "<Space>f", "<cmd>lua require'telescope.builtin'.find_files{}<CR>", opts)
+  -- vim.api.nvim_set_keymap("n", "<Space>f", "<cmd>lua require'telescope.builtin'.find_files{}<CR>", opts)
+  vim.api.nvim_set_keymap("n", "<Space>f", "<cmd>lua require'conf.telescope'.find_files{}<CR>", opts)
   vim.api.nvim_set_keymap("n", "<Space>p", "<cmd>lua require'telescope.builtin'.git_files{ shorten_path = true }<CR>", opts)
   vim.api.nvim_set_keymap("n", "<Space>o", "<cmd>lua require'telescope.builtin'.oldfiles{}<CR>", opts)
   vim.api.nvim_set_keymap("n", "<Space>r", "<cmd>lua require'telescope.builtin'.lsp_references{}<CR>", opts)
