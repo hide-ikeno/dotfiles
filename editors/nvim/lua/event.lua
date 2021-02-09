3f4e87f578c619edacfe1b100a4c8abb68e73e50
Hidekazu Ikeno <hide.ikeno@gmail.com>
2021-02-09 22:56:29 +0900

    nvim: add util function to deal with augroups

---
+++ editors/nvim/lua/event.lua
@@ -0,0 +1,46 @@
+local M = {
+  _funcs = {},
+  name = (function()
+    local file = debug.getinfo(1, 'S').source
+    return file:match('/(%a+)%.lua$')
+  end)()
+}
+
+function M.create_augroups(groups)
+  vim.validate{ groups = {groups, "table"} }
+  for name, definitions in pairs(groups) do
+    vim.validate{
+      name = {name, "string"},
+      definitions = {definitions, "table"},
+    }
+    local cmds = {}
+    for _, def in ipairs(definitions) do
+      vim.validate{
+        def = {
+          def,
+          function()
+            return type(def) == 'table' and vim.tbl_count(d) == 3
+          end,
+          "each definition should contain 3 values"
+      }
+      local events, patterns, cmd_or_func = def[1], def[2], def[3]
+      local command
+      if type(cmd_or_func) == "string" then
+        command = cmd_or_func
+      else
+        table.insert(M._funcs, cmd_or_func)
+        command = ([[lua require'%s'._funcs[%d]()]]):format(M.name, #M._funcs)
+      end
+      table.insert(cmds, string.format("autocmd %s %s %s", events, patterns, command))
+    end
+
+    vim.cmd("augroup " .. name)
+    vim.cmd("autocmd!")
+    for _, command in ipairs(cmds) do
+      vim.cmd(command)
+    end
+    vim.cmd('augroup END')
+  end
+end
+
+return M
